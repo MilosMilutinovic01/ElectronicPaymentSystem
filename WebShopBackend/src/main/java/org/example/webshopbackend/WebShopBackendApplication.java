a61757b0774c05037d01742251116f297262e9b0
Milos Milutinovic <milosmilutinovic6@gmail.com>
2024-11-10 13:38:49 +0100

    Implemented autentification

--- WebShopBackend/src/main/java/org/example/webshopbackend/WebShopBackendApplication.java
+++ WebShopBackend/src/main/java/org/example/webshopbackend/WebShopBackendApplication.java
@@ -1,9 +1,12 @@
 package org.example.webshopbackend;
 
+import org.example.webshopbackend.config.RsaKeyConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyConfigProperties.class)
 public class WebShopBackendApplication {
 
 	public static void main(String[] args) {
--- WebShopFrontend/src/main.ts
+++ WebShopFrontend/src/main.ts
@@ -1,9 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import {provideHttpClient} from '@angular/common/http';
+import {routes} from './app/app.routes';
+import {provideRouter} from '@angular/router';
+import {importProvidersFrom} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ToastrModule} from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom(FormsModule, ReactiveFormsModule, ToastrModule.forRoot(), BrowserAnimationsModule),
   ]
 });
