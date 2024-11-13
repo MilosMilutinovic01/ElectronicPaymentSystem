86782bfbc9d75b19aa9c12d24e4b4c697fe02c53
Milos Milutinovic <milosmilutinovic6@gmail.com>
2024-11-13 18:24:46 +0100

    Fixed interceptor

--- PSP/PSPFrontend/src/main.ts
+++ PSP/PSPFrontend/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
@@ -9,10 +9,11 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CheckboxModule } from 'primeng/checkbox';
+import {authInterceptor} from "./app/shared/interceptors/auth.interceptor";
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes),
     importProvidersFrom(
       FormsModule,
